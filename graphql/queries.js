bf3ca9216b680b1d24a61b9d6aff41d3905f1b0f
ChickyMasala <samuel.goh96@gmail.com>
2024-01-24 18:17:38 +0800

    codegen

--- graphql/queries.js
+++ graphql/queries.js
@@ -1,12 +1,51 @@
 /* eslint-disable */
 // this is an auto generated file. This will be overwritten
 
+export const getMonthRecord = /* GraphQL */ `
+  query GetMonthRecord($id: ID!) {
+    getMonthRecord(id: $id) {
+      id
+      startDate
+      maxSpending
+      Expenses {
+        nextToken
+        __typename
+      }
+      currentSpending
+      createdAt
+      updatedAt
+      __typename
+    }
+  }
+`;
+export const listMonthRecords = /* GraphQL */ `
+  query ListMonthRecords(
+    $filter: ModelMonthRecordFilterInput
+    $limit: Int
+    $nextToken: String
+  ) {
+    listMonthRecords(filter: $filter, limit: $limit, nextToken: $nextToken) {
+      items {
+        id
+        startDate
+        maxSpending
+        currentSpending
+        createdAt
+        updatedAt
+        __typename
+      }
+      nextToken
+      __typename
+    }
+  }
+`;
 export const getExpense = /* GraphQL */ `
   query GetExpense($id: ID!) {
     getExpense(id: $id) {
       id
       value
       description
+      monthrecordID
       createdAt
       updatedAt
       __typename
@@ -24,6 +63,36 @@ export const listExpenses = /* GraphQL */ `
         id
         value
         description
+        monthrecordID
+        createdAt
+        updatedAt
+        __typename
+      }
+      nextToken
+      __typename
+    }
+  }
+`;
+export const expensesByMonthrecordID = /* GraphQL */ `
+  query ExpensesByMonthrecordID(
+    $monthrecordID: ID!
+    $sortDirection: ModelSortDirection
+    $filter: ModelExpenseFilterInput
+    $limit: Int
+    $nextToken: String
+  ) {
+    expensesByMonthrecordID(
+      monthrecordID: $monthrecordID
+      sortDirection: $sortDirection
+      filter: $filter
+      limit: $limit
+      nextToken: $nextToken
+    ) {
+      items {
+        id
+        value
+        description
+        monthrecordID
         createdAt
         updatedAt
         __typename
